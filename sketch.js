14a4e4216355f9a396da552f2829fcaf1ffd436d
Akshatj11 <62606542+Akshatj11@users.noreply.github.com>
2020-03-31 19:47:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var fixedRect,movingRect;
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor="green";
+  fixedRect.velocityX=-2;
+
+  movingRect=createSprite(400,400,80,30);
+  movingRect.shapeColor="green";
+  movingRect.velocityX=2;
+}
+
+function draw() {
+  background(0); 
+ 
+  
+  bounceOff(fixedRect,movingRect);
+  
+  drawSprites();
+}
